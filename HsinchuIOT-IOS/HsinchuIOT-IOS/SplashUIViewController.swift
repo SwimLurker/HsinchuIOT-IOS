623108219931c9128e6620725326a310b80b0a70
Chandler Li <chandlerli@Ivens-MacBook-Pro.local>
2015-09-18 01:51:40 +0800

    first commit

---
+++ HsinchuIOT-IOS/HsinchuIOT-IOS/SplashUIViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SplashUIViewController.swift
+//  HsinchuIOT-IOS
+//
+//  Created by Chandler Li on 9/18/15.
+//  Copyright © 2015 SL Studio. All rights reserved.
+//
+
+import UIKit
+
+class SplashUIViewController: UIViewController {
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        startAnimation()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func startAnimation(){
+        //splashImage.startAnimating()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
